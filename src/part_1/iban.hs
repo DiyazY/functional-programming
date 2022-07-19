9dbd945e9704ce980ac735d485c7c17750e37361
Diyaz Yakubov <diyaz.yakubov@wunderdog.fi>
2022-07-19 23:02:26 +0300

    learned guards and using of other functions

--- src/part_1/iban.hs
+++ src/part_1/iban.hs
@@ -12,3 +12,33 @@ Finnish IBAN begins with the country code FI.
 All the characters after the country code are digits.
 You can assume that the input is without whitespaces.
 -}
+validate :: String -> Bool
+validate x 
+    | y <- x, length y == 18,
+    (y !! 0 == 'F' && y !! 1 == 'I'),
+     onlyDigits (tail (tail y)),
+     ((read (moveCharsToEnd(replace' y)) :: Integer) `mod` 97 == 1) = True
+    | otherwise = False
+
+
+
+onlyDigits :: String -> Bool
+--Checks whether given list consists only of digits.
+onlyDigits [] = False
+onlyDigits [s] = isDigit s
+onlyDigits (x:xs)
+    | isDigit x = onlyDigits xs
+    | otherwise = False
+
+replace' :: String -> String
+--Replaces the country code FI with 1518 at the beginning of the string.
+replace' xs | x <- xs = '1':'5':'1':'8' : drop 2 xs
+
+isDigit :: Char -> Bool
+isDigit c = c >= '0' && c <= '9'
+
+--Takes the first 6 (4) characters and puts them to end of the string.
+--6 because I have converted FI to 1518, so that's two additional chars.
+moveCharsToEnd :: String -> String
+moveCharsToEnd x | x <- x = drop 6 x ++ take 6 x
+
