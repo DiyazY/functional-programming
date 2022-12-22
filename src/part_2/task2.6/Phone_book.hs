d45c8a71753d2e35ad24b417e38c96c7647e1b2c
diyaz yakubov <diyaz.yakubov@disqo.com>
2022-12-22 21:33:55 +0200

    learned modules export/import

---
+++ src/part_2/task2.6/Phone_book.hs
@@ -0,0 +1,18 @@
+module Phone_book
+( addEntry
+, findEntries
+) where
+
+import Phone_type2
+
+data PhoneBookEntry = PhoneBookEntry { name :: String , phone :: Phone } deriving(Eq, Ord, Show)
+type PhoneBook = [PhoneBookEntry]
+
+findEntries :: String -> PhoneBook -> PhoneBook
+findEntries givenName book = [entry | entry<-book, (name entry) == givenName]
+
+addEntry :: String -> String -> String -> String -> PhoneBook -> PhoneBook
+addEntry name pt cc pn book 
+    | elem entry book = book  -- check if the entry already exists
+    | otherwise = [entry] ++ book  -- add the new entry to the phone book
+    where entry = PhoneBookEntry { name = name, phone = readPhone pt cc pn }
